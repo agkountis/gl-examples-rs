0786b60a2d1ac4c17ab3c7297092dd8f3f35c506
Angelos Gkountis <angelos.gkountis@gmail.com>
2021-11-08 16:05:58 +0000

    Upgrades image crate. Adds memoffset dependency.

--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -1,12 +1,6 @@
-#[macro_use]
-macro_rules! offset_of {
-    ($ty:ty, $field:ident) => {
-        unsafe { &(*(0 as *const $ty)).$field as *const _ as usize }
-    };
-}
-
 pub mod buffer;
 pub mod color;
+pub mod device;
 pub mod format;
 pub mod framebuffer;
 pub mod light;
@@ -18,7 +12,6 @@ pub mod sampler;
 pub mod shader;
 pub mod state;
 pub mod texture;
-pub mod device;
 
 pub trait Draw {
     fn draw(&self);
